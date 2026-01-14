3b901aecf6b5e95534b18e6dc31443627d06a549
ahmetbayrak19-afk <damlagelmez8@gmail.com>
2026-01-14 10:36:50 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/ogrenim/app/MainActivity.java
+++ android/app/src/main/java/com/ogrenim/app/MainActivity.java
@@ -1,1 +1,60 @@
+package com.ogrenim.app; // <-- ARTIK DOĞRUSU BU
+
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        
+        // --- AYARLAR ---
+        settings.setJavaScriptEnabled(true);        
+        settings.setDomStorageEnabled(true);       
+        settings.setAllowFileAccess(true);          
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        // --- MİKROFON İZİNLERİNİ OTOMATİK ONAYLAYAN KOD ---
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false; 
+            }
+        });
+        
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
 
