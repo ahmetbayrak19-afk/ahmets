73f94229d7da17eff59e540f850aea92d349f0a2
ahmetbayrak19-afk <damlagelmez8@gmail.com>
2026-01-25 04:35:55 +0300

    Update Assets.ts

--- client/src/aba/esle/game/Assets.ts
+++ client/src/aba/esle/game/Assets.ts
@@ -1,28 +1,27 @@
 // Assets.ts
-// Resimlerin hepsi bu dosya ile AYNI klasörde olsun (veya yolunu ./balik/d1.png gibi düzelt)
 
-// YÜZME (D)
-import d1 from './d1.png'; import d2 from './d2.png'; import d3 from './d3.png';
-import d4 from './d4.png'; import d5 from './d5.png'; import d6 from './d6.png';
-import d7 from './d7.png'; import d8 from './d8.png';
+// 1. BALIK RESİMLERİ (balik klasörünün içinde)
+import d1 from './balik/d1.png'; import d2 from './balik/d2.png'; import d3 from './balik/d3.png';
+import d4 from './balik/d4.png'; import d5 from './balik/d5.png'; import d6 from './balik/d6.png';
+import d7 from './balik/d7.png'; import d8 from './balik/d8.png';
 
-// DÖNÜŞ (L - Left) - Sola Dönüş Animasyonu
-import l1 from './l1.png'; import l2 from './l2.png'; import l3 from './l3.png';
-import l4 from './l4.png'; import l5 from './l5.png'; import l6 from './l6.png';
-import l7 from './l7.png';
+// Dönüş Animasyonu (l1...l7)
+import l1 from './balik/l1.png'; import l2 from './balik/l2.png'; import l3 from './balik/l3.png';
+import l4 from './balik/l4.png'; import l5 from './balik/l5.png'; import l6 from './balik/l6.png';
+import l7 from './balik/l7.png';
 
-// YEME (Y)
-import y1 from './y1.png'; import y2 from './y2.png'; import y3 from './y3.png';
-import y4 from './y4.png'; // Eğer y4 varsa ekle, yoksa listeden çıkar
+// Yeme Animasyonu (y1...y4)
+import y1 from './balik/y1.png'; import y2 from './balik/y2.png'; import y3 from './balik/y3.png';
+import y4 from './balik/y4.png'; 
 
-// ÇEVRE
+// 2. ÇEVRE RESİMLERİ (Assets.ts ile yan yana)
 import geceImg from './gece.png';
 import anazemin from './anazemin.png'; 
 import zemin from './zemin.png';
 
 // LİSTELER
 const SWIM_SRCS = [d1, d2, d3, d4, d3, d2, d1, d5, d6, d7, d8, d7, d6, d5];
-const TURN_LEFT_SRCS = [l1, l2, l3, l4, l5, l6, l7]; // Dönüş animasyonu
+const TURN_LEFT_SRCS = [l1, l2, l3, l4, l5, l6, l7];
 const EAT_SRCS = [y1, y2, y3, y4, y3, y2, y1]; 
 
 export type AssetLibrary = {
