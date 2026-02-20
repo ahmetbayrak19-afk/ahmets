c5564b4b6ee97f7d13dd2d86bbeddc112b4f82af
ahmetbayrak19-afk <damlagelmez8@gmail.com>
2026-02-20 23:27:18 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     // Yerel dosyaların 'https://localhost/' üzerinden sunulmasını sağlar
-    androidScheme: 'https',
+    androidScheme: 'http',
   },
 };
 
