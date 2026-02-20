3b90cd449ee2f415d7c6770966b1044a3eaf382c
ahmetbayrak19-afk <damlagelmez8@gmail.com>
2026-02-20 17:01:04 +0300

    Update DenizBackground.tsx

--- client/src/aba/esle/game/DenizBackground.tsx
+++ client/src/aba/esle/game/DenizBackground.tsx
@@ -1,16 +1,10 @@
-import React, { Suspense, useRef, useEffect, useMemo } from "react";
-import { Canvas, useFrame, extend, useThree, useLoader } from "@react-three/fiber";
-import { useGLTF, Environment } from "@react-three/drei";
+import React, { Suspense, useMemo, useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
 import * as THREE from "three";
-import { Water } from "three-stdlib";
 import { Fish3D } from "./Fish3D";
 
-// 🔥 İŞTE BURADA: Denizi doğrudan import ediyoruz!
-import denizModelUrl from "./deniz.glb";
-
-const WATER_NORMALS_URL = "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/water/Water_1_M_Normal.jpg";
-
-extend({ Water });
+// DENİZ VE OKYANUS (WATER) İLE İLGİLİ HER ŞEYİ KÖKTEN SİLDİM!
 
 function BackgroundManager({ cameraRef }: { cameraRef: any }) {
   const { scene } = useThree();
@@ -50,51 +44,6 @@ function CameraRig({ fishRef }: { fishRef: any }) {
   return null;
 }
 
-function Ocean() {
-  const refTop = useRef<any>();
-  const refBottom = useRef<any>(); 
-  const gl = useThree((s) => s.gl);
-  const waterNormals = useLoader(THREE.TextureLoader, WATER_NORMALS_URL);
-  
-  useEffect(() => {
-    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-    waterNormals.repeat.set(4, 4);
-  }, [waterNormals]);
-
-  const config = useMemo(() => ({
-      textureWidth: 512, textureHeight: 512, waterNormals,
-      sunDirection: new THREE.Vector3(), sunColor: 0x0077ff, waterColor: 0x001e0f,
-      distortionScale: 3.7, fog: true, format: gl.outputColorSpace === "srgb" ? THREE.SRGBColorSpace : undefined,
-    }), [waterNormals, gl.outputColorSpace]);
-
-  useFrame((_, delta) => {
-    const t = delta * 0.5;
-    if (refTop.current) refTop.current.material.uniforms.time.value += t;
-    if (refBottom.current) refBottom.current.material.uniforms.time.value += t;
-  });
-
-  return (
-    <group position={[0, 15, 0]}>
-      {/* @ts-ignore */}
-      <water ref={refTop} args={[new THREE.PlaneGeometry(20000, 20000), config]} rotation={[-Math.PI / 2, 0, 0]} />
-      {/* @ts-ignore */}
-      <water 
-        ref={refBottom} 
-        args={[new THREE.PlaneGeometry(20000, 20000), config]} 
-        rotation={[Math.PI / 2, 0, 0]} 
-        position={[0, -0.05, 0]}       
-      />
-    </group>
-  );
-}
-
-function DenizModel() {
-  // Import ettiğimiz değişkeni kullanıyoruz
-  const { scene } = useGLTF(denizModelUrl);
-  const clone = useMemo(() => scene.clone(), [scene]);
-  return <primitive object={clone} scale={[5, 5, 5]} position={[0, -20, -10]} rotation={[0, -Math.PI / 2, 0]} />;
-}
-
 export default function DenizBackground({ cameraRef, fishRef }: { cameraRef: any, fishRef: any }) {
   return (
     <div className="absolute inset-0 bg-black">
@@ -111,16 +60,12 @@ export default function DenizBackground({ cameraRef, fishRef }: { cameraRef: any
         <ambientLight intensity={1.5} color="#ffffff" />
         <directionalLight position={[10, 20, 10]} intensity={2} color="#ffffff" castShadow />
 
+        {/* SAHNEDE SADECE VE SADECE BALIK VAR! */}
         <Suspense fallback={null}>
-            <DenizModel />
-            <Ocean />
-            <Fish3D fishRef={fishRef} />
+          <Fish3D fishRef={fishRef} />
         </Suspense>
+
       </Canvas>
     </div>
   );
-}
-
-// Preload kısmına da aynı değişkeni veriyoruz
-useGLTF.preload(denizModelUrl);
-      
+      }
